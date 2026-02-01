ac10cf6e9f0ade8271d2808a5a663601114f3cf3
ShyamGtm-commits <gautamshyam595@gmail.com>
2026-02-01 17:13:49 +0545

    Cleaned up Task and Internship models and migrations

--- api/models.py
+++ api/models.py
@@ -147,12 +147,12 @@ class InternshipApplication(models.Model):
         related_name = 'applications'
     )
     
-    user = models.OneToOneField(
+    user = models.ForeignKey( 
         settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        related_name='application'
+        related_name='applications' 
     )
 
     status = models.CharField(
@@ -182,3 +182,44 @@ class InternshipApplication(models.Model):
     def __str__(self):
         return f"{self.full_name} - {self.internship}"
 
+class Task(models.Model):
+    TASK_STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('in_progress', 'In_progress'),
+        ('completed', 'Completed'),
+        ('overdue', 'Overdue')
+    )
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    assigned_to = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='assigned_tasks'
+    )
+
+    assigned_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='created_tasks'
+    )
+
+    status = models.CharField(
+        max_length=20,
+        choices=TASK_STATUS_CHOICES,
+        default='pending'
+    )
+    due_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    submission_link = models.URLField(blank=True, null=True)
+    admin_feedback = models.TextField(blank=True, null=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.title} - {self.assigned_to.email}"
