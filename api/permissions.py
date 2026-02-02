036a87f9ec7f9f9999bec6e842c2ae1142c1eb63
ShyamGtm-commits <gautamshyam595@gmail.com>
2026-02-02 23:16:21 +0545

    Fixed the applications(plural) error in permissions

--- api/permissions.py
+++ api/permissions.py
@@ -14,7 +14,7 @@ class IsIntern(permissions.BasePermission):
         return bool(
             request.user and 
             request.user.is_authenticated and 
-            hasattr(request.user, 'application') and 
+            request.user.applications.exists() and 
             not request.user.is_staff
         )
     
@@ -34,10 +34,7 @@ class IsOwner(permissions.BasePermission):
 class IsAcceptedIntern(permissions.BasePermission):
     # allows access if the internship  is accepted
     def has_permission(self, request, view):
-        
         if not (request.user and request.user.is_authenticated):
             return False
         
-        application = getattr(request.user, 'application', None)
-        
-        return application is not None and application.status == 'accepted'
+        return request.user.applications.filter(status='accepted').exists()
